aec198d7f4d0e48c58a6878f5e2c28a2e044e3d4
pingjejovup <pingjejovup@gmail.com>
2021-04-21 00:03:24 +0800

    update week2 hw

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,7 @@
 function printStars(n) {
-
+  for (let i = 0; i < n && n <= 30; i++) {
+    console.log('*');    
+  }
 }
 
-printStars(5)
+printStars(5);
--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,15 @@
 function capitalize(str) {
-  
+  if(str){
+    let strArray = str.split("");
+    let firstChar = strArray[0];
+    if (firstChar.match(/[a-z]/i)){
+      firstChar = firstChar.toUpperCase();
+      strArray.splice(0, 1, firstChar);      
+      return strArray.join('');
+    }else{
+      return str
+    }
+  }
 }
 
 console.log(capitalize('hello'));
