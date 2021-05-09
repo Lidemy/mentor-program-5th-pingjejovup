2b1c2db37b8b5a1490197981f0141e320f6ba67f
pingjejovup <pingjejovup@gmail.com>
2021-05-09 10:56:30 +0800

    week02 revise

--- homeworks/week2/challenge.js
+++ homeworks/week2/challenge.js
@@ -1,38 +1,20 @@
 function search(arr, n) {
-  let middle = Math.round((arr.length - 1) / 2);
-  var times = Math.round((arr.length + 1) / 2);
-  console.log('midd', middle);
-  console.log('rn', arr.indexOf(n));
-  if (n == arr[0]) {
+  let middle = Math.round((arr.length - 1) / 2)
+  const times = Math.round((arr.length + 1) / 2)
+  if (n === arr[0]) {
     return 0
   }
 
   for (let i = 0; i < times; i++) {
-    if (n == arr[middle]) {
+    if (n === arr[middle]) {
       return middle
     } else if (n > arr[middle]) {
-
-      middle = Math.round((arr.length - 1 + middle) / 2);
-      // console.log('b', middle);
-
+      middle = Math.round((arr.length - 1 + middle) / 2)
     } else {
-      middle = Math.round((middle) / 2);
-      // console.log('s', middle);
+      middle = Math.round((middle) / 2)
     }
-    
   }
-
-  
-  // return arr.indexOf(n)
   return -1
 }
 
-
-// 3
-// console.log(search([1, 3, 10, 14, 39], 1));
-// console.log(search([1, 3, 10, 14, 39], 3));
-// console.log(search([1, 3, 10, 14, 39], 10));
-// console.log(search([1, 3, 10, 14, 39], 14));
-// console.log(search([1, 3, 10, 14, 39], 39));
-// console.log(search([1, 3, 10, 14, 39], 45));
-
+console.log(search([1, 3, 10, 14, 39], 14))
--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,7 +1,7 @@
 function printStars(n) {
-  for (let i = 0; i < n && n <= 30; i++) {
-    console.log('*');    
+  for (let i = 0; i < n; i++) {
+    console.log('*')
   }
 }
 
-printStars(5);
+printStars(5)
